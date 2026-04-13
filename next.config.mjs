9c9fd9164814ca46756a4d8a0983b32cfeaf2311
DropBoost <dev@dropboost.it>
2026-04-13 19:14:17 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb", // aumenta per upload
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vekpjqniuxntcwftbmzu.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
