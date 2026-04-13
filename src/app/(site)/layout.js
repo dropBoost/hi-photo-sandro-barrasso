e5069d42294bdfc09ccbd5be11d9eeba0726ca92
DropBoost <dev@dropboost.it>
2026-04-13 19:37:29 +0200

    aggiunta delle analytics vercel

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -1,6 +1,7 @@
 import { getSettings } from "@/lib/setting";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export default async function Layout({ children }) {
 
@@ -9,6 +10,7 @@ export default async function Layout({ children }) {
   return (
   <>
   <div className="h-screen w-screen flex flex-col justify-between overflow-auto">
+    <Analytics/>
     <Header/>
     <div className="flex-1">
       {children}
